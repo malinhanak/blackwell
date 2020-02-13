a420d9c841ed38d98a523b1ccda325d9c6748efb
Malin Hanak <malinehanak@gmail.com>
2020-02-13 15:11:04 +0100

    GeneralButton component
    Button to be used when it's just normal button with no icon or special purpose.

--- src/lib/styles/index.js
+++ src/lib/styles/index.js
@@ -1,5 +1,6 @@
 export { Input as InputStyle, InputWrapper } from './Form/Input';
 export { DiceButton } from './Buttons/DiceButton';
+export { GeneralButton } from './Buttons/GeneralButton.js';
 export { Form } from './Form/Form';
 export { Datalist } from './Form/Datalist';
 export { GlobalStyle } from './GlobalStyle';
