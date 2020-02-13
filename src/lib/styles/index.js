c9592aaea831e232f14b74bfd7472cd510d73945
Malin Hanak <malinehanak@gmail.com>
2020-02-13 14:37:14 +0100

    DiceButton to new folder

--- src/lib/styles/index.js
+++ src/lib/styles/index.js
@@ -1,5 +1,5 @@
 export { Input as InputStyle, InputWrapper } from './Form/Input';
-export { DiceButton } from './DiceButton';
+export { DiceButton } from './Buttons/DiceButton';
 export { Form } from './Form/Form';
 export { Datalist } from './Form/Datalist';
 export { GlobalStyle } from './GlobalStyle';
