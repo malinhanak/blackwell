d19f77de7ea3423f1542485bb32ee275d5b4e460
Malin Hanak <malinehanak@gmail.com>
2020-02-13 14:21:53 +0100

    Removed some unneeded styles

--- src/lib/styles/Form/Datalist.js
+++ src/lib/styles/Form/Datalist.js
@@ -3,8 +3,4 @@ import styled from 'styled-components';
 export const Datalist = styled.datalist`
   border-radius: none;
   border: none;
-  :hover,
-  :active {
-    background: ${(props) => props.theme.colors.accent};
-  }
 `;
