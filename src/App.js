e46d14610ea998363be62922d2f7b64d244f1684
Malin Hanak <malinehanak@gmail.com>
2020-02-19 15:57:20 +0100

    Routing setup with suspense and fallback.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import { CreateNewGame } from './components/games';
+import React, { lazy, Suspense } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { LoadingOverlay } from './assets/Loading';
+
+// Lazy Load imports.
+const CreateNewGame = lazy(() => import('./components/games/CreateNewGame'));
 
 export const App = () => {
   return (
-    <>
-      <h1>Hello DiceTrax!</h1>
-      <CreateNewGame />
-    </>
+    <Suspense fallback={<LoadingOverlay />}>
+      <Switch>
+        <Route exact path='/' render={() => <h1>Hello DiceTrax!</h1>} />
+        <Route path='/new-game' component={CreateNewGame} />
+      </Switch>
+    </Suspense>
   );
 };
